417e55cfffceb328ecff22b3736bfbd21fffc5c4
Owallah <mikesonowallah@gmail.com>
2025-02-04 23:34:30 +0300

    Worked on the scrollup function and header touches

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -5,7 +5,15 @@ import { Avatar } from '@mui/material'
 import ProfilePic from "../../assets/profile.jpg"
 
 const Header = () => {
+    window.addEventListener("scroll", function () {
+        const header = document.querySelector(".header");
+        //  when the scrolling is above 200 viewport Height, add the show_scroll className to the scrollup element
+        if (this.scrollY >= 80) header.classList.add("show_header");
+        else header.classList.remove("show_header");
+      });
+
     const [ toggle, setToggle ] = useState(false)
+    const [ activeNav, setActiveNav ] = useState("#home")
   return (
     <header className='header'>
         <nav className='nav container flexCenterSpaceBtwn'>
@@ -16,7 +24,7 @@ const Header = () => {
             <div className={toggle ? "nav_menu show_menu" : "nav_menu"}>
                 <ul className="nav_list">
                     <li className="nav_item">
-                        <a href="" className='nav_link active-link flexColCenter'>
+                        <a href="#home" onClick={() => setActiveNav("#home")} className={activeNav === "#home" ? 'nav_link active-link flexColCenter' : 'nav_link flexColCenter'}>
                             <div className="nav_icon">
                                 <Home />
                             </div>
@@ -24,7 +32,7 @@ const Header = () => {
                         </a>
                     </li>
                     <li className="nav_item">
-                        <a href="" className='nav_link flexColCenter'>
+                        <a href="#about" onClick={() => setActiveNav("#about")} className={activeNav === "#about" ? 'nav_link active-link flexColCenter' : 'nav_link flexColCenter'}>
                         <div className="nav_icon">
                             <Person /> 
                             </div>
@@ -32,7 +40,7 @@ const Header = () => {
                         </a>
                     </li>
                     <li className="nav_item">
-                        <a href="" className='nav_link flexColCenter'>
+                        <a href="#skills" onClick={() => setActiveNav("#skills")} className={activeNav === "#skills" ? 'nav_link active-link flexColCenter' : 'nav_link flexColCenter'}>
                         <div className="nav_icon">
                             <Work className='nav_icon' /> 
                             </div>
@@ -40,7 +48,7 @@ const Header = () => {
                         </a>
                     </li>
                     <li className="nav_item">
-                        <a href="" className='nav_link flexColCenter'>
+                        <a href="#services" onClick={() => setActiveNav("#services")} className={activeNav === "#services" ? 'nav_link active-link flexColCenter' : 'nav_link flexColCenter'}>
                         <div className="nav_icon">
                             <DesignServices className='nav_icon' /> 
                             </div>
@@ -48,7 +56,7 @@ const Header = () => {
                         </a>
                     </li>
                     <li className="nav_item">
-                        <a href="" className='nav_link flexColCenter'>
+                        <a href="#portfolio" onClick={() => setActiveNav("#portfolio")} className={activeNav === "#portfolio" ? 'nav_link active-link flexColCenter' : 'nav_link flexColCenter'}>
                         <div className="nav_icon">
                             <AccountTree className='nav_icon' /> 
                             </div>
@@ -56,7 +64,7 @@ const Header = () => {
                         </a>
                     </li>
                     <li className="nav_item">
-                        <a href="" className='nav_link flexColCenter'>
+                        <a href="#contact" onClick={() => setActiveNav("#contact")} className={activeNav === "#contact" ? 'nav_link active-link flexColCenter' : 'nav_link flexColCenter'}>
                         <div className="nav_icon">
                             <ContactSupport className='nav_icon' /> 
                             </div>
---
+++ src/components/scrollup/Scrollup.jsx
@@ -0,0 +1,20 @@
+import { ArrowUpwardOutlined } from "@mui/icons-material";
+import "./Scrollup.css";
+
+const Scrollup = () => {
+  window.addEventListener("scroll", function () {
+    const scrollUp = document.querySelector(".scrollup");
+    //  when the scrolling is above 560 viewport Height, add the show_scroll className to the scrollup element
+    if (this.scrollY >= 560) scrollUp.classList.add("show_scroll");
+    else scrollUp.classList.remove("show_scroll");
+  });
+  return (
+    <a href="#" className="scrollup">
+      <div className="scrollup_icon">
+        <ArrowUpwardOutlined />
+      </div>
+    </a>
+  );
+};
+
+export default Scrollup;
